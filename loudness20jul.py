9f846d607c4635b0d5c2c80bcd710d7145f0bb34
Bernd Porr <mail@berndporr.me.uk>
2022-07-20 06:54:53 +0100

    Added 20th Jul

---
+++ loudness20jul.py
@@ -0,0 +1,43 @@
+#!/usr/bin/python3
+import scipy.io.wavfile as wavfile
+import matplotlib.pyplot as plt
+import scipy
+import numpy as np
+from scipy import signal
+import datetime
+
+def detthuds(fs,customdate,y):
+    thudno = 1
+    cooloff = 0
+    for i in range(len(y)):
+        if (y[i] > 0.06) and (cooloff < 1):
+           x = customdate + datetime.timedelta(seconds=i/fs)
+           print("Thud #{} at {}".format(thudno,x))
+           thudno = thudno + 1
+           cooloff = fs
+        if cooloff > 0:
+            cooloff = cooloff - 1
+
+
+f = wavfile.read('STE-20Jul_5_02am_500Hz.wav');
+fs = f[0]
+y = f[1] / 32768.0
+
+y = np.abs(y)
+
+customdate = datetime.datetime(2022, 7, 20, 5, 2)
+x = [customdate + datetime.timedelta(seconds=i/fs) for i in range(len(y))]
+
+t = "Loudness - "+customdate.strftime("%c")
+
+print(t)
+detthuds(fs,customdate,y)
+
+plt.figure(t)
+plt.title(t)
+plt.plot(x,y)
+plt.xlabel("Date/time")
+plt.ylabel("Loudness (Zoom H4, sens high)")
+plt.ylim([0,1])
+
+plt.show()
